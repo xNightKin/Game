233741bce6209391e7f679c93a79a4edaf05bb90
xNightKin <viethoa1996@gmail.com>
2017-02-07 21:13:12 +0200

    no uid

--- src/Game.java
+++ src/Game.java
@@ -4,8 +4,6 @@ import java.util.Random;
 
 public class Game extends Canvas implements Runnable{
 
-    private static final long serialVersionUID = -1442798787354930462L;
-
     public static final int WIDTH = 640, HEIGHT = WIDTH/12*9;
 
     public Thread thread;
